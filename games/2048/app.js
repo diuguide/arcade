f9c97b865d4d5a7aeb3ef79ebe25b1a850fbfd5f
diuguide <61917141+diuguide@users.noreply.github.com>
2020-08-17 20:33:11 -0400

    added number movement functionality(left, right)

--- games/2048/app.js
+++ games/2048/app.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
             squares.push(square)
         }
         generate()
+        generate()
     }
     createBoard()
     // generate a random number
@@ -23,5 +24,59 @@ document.addEventListener('DOMContentLoaded', () => {
             squares[randomNumber].innerHTML = 2
         } else generate()
     }
+
+    function moveRight() {
+        for (var i = 0; i < 16; i++) {
+            if (i % 4 === 0) {
+                let totalOne = squares[i].innerHTML
+                let totalTwo = squares[i + 1].innerHTML
+                let totalThree = squares[i + 2].innerHTML
+                let totalFour = squares[i + 3].innerHTML
+                let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+                console.log(row)
+
+                let filteredRow = row.filter(num => num)
+                console.log(filteredRow)
+                let missing = 4 - filteredRow.length
+                let zeros = Array(missing).fill(0)
+                console.log(zeros)
+                let newRow = zeros.concat(filteredRow)
+                console.log(newRow)
+
+                squares[i].innerHTML = newRow[0]
+                squares[i + 1].innerHTML = newRow[1]
+                squares[i + 2].innerHTML = newRow[2]
+                squares[i + 3].innerHTML = newRow[3]
+            }
+        }
+    }
+
+
+    function moveLeft() {
+        for (var i = 0; i < 16; i++) {
+            if (i % 4 === 0) {
+                let totalOne = squares[i].innerHTML
+                let totalTwo = squares[i + 1].innerHTML
+                let totalThree = squares[i + 2].innerHTML
+                let totalFour = squares[i + 3].innerHTML
+                let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+                console.log(row)
+
+                let filteredRow = row.filter(num => num)
+                console.log(filteredRow)
+                let missing = 4 - filteredRow.length
+                let zeros = Array(missing).fill(0)
+                console.log(zeros)
+                let newRow = filteredRow.concat(zeros)
+                console.log(newRow)
+
+                squares[i].innerHTML = newRow[0]
+                squares[i + 1].innerHTML = newRow[1]
+                squares[i + 2].innerHTML = newRow[2]
+                squares[i + 3].innerHTML = newRow[3]
+            }
+        }
+    }
+    moveLeft()
 
 })
