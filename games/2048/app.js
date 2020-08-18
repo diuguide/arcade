e2691be56544713ce6e479f772493f45ddf8d798
diuguide <61917141+diuguide@users.noreply.github.com>
2020-08-17 23:21:38 -0400

    app centered, still having trouble with base math coding

--- games/2048/app.js
+++ games/2048/app.js
@@ -136,46 +136,51 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function moveDown() {
         for (var i = 0; i < 4; i++) {
-            if (i % 4 === 0) {
-                let totalOne = squares[i].innerHTML
-                let totalTwo = squares[i + width].innerHTML
-                let totalThree = squares[i + (width * 2)].innerHTML
-                let totalFour = squares[i + (width * 3)].innerHTML
-                let column = [parseInt(totalOne) + parseInt(totalTwo) + parseInt(totalThree) + parseInt(totalFour)]
 
-                let filteredColumn = column.filter(num => num)
-                let missing = 4 - filteredColumn.length
-                let zeros = Array(missing).fill(0)
-                let newColumn = zeros.concat(filteredColumn)
+            let totalOne = squares[i].innerHTML
+            let totalTwo = squares[i + width].innerHTML
+            let totalThree = squares[i + (width * 2)].innerHTML
+            let totalFour = squares[i + (width * 3)].innerHTML
+            let column = [parseInt(totalOne) + parseInt(totalTwo) + parseInt(totalThree) + parseInt(totalFour)]
 
-                squares[i].innerHTML = newColumn[0]
-                squares[i + width].innerHTML = newColumn[1]
-                squares[i + (width * 2)].innerHTML = newColumn[2]
-                squares[i + (width * 3)].innerHTML = newColumn[3]
-            }
+            let filteredColumn = column.filter(num => num)
+            let missing = 4 - filteredColumn.length
+            let zeros = Array(missing).fill(0)
+            let newColumn = zeros.concat(filteredColumn)
+
+            squares[i].innerHTML = newColumn[0]
+            squares[i + width].innerHTML = newColumn[1]
+            squares[i + (width * 2)].innerHTML = newColumn[2]
+            squares[i + (width * 3)].innerHTML = newColumn[3]
+
         }
 
     }
 
     function moveUp() {
         for (var i = 0; i < 4; i++) {
-            if (i % 4 === 0) {
-                let totalOne = squares[i].innerHTML
-                let totalTwo = squares[i + width].innerHTML
-                let totalThree = squares[i + (width * 2)].innerHTML
-                let totalFour = squares[i + (width * 3)].innerHTML
-                let column = [parseInt(totalOne) + parseInt(totalTwo) + parseInt(totalThree) + parseInt(totalFour)]
 
-                let filteredColumn = column.filter(num => num)
-                let missing = 4 - filteredColumn.length
-                let zeros = Array(missing).fill(0)
-                let newColumn = filteredColumn.concat(zeros)
+            let totalOne = squares[i].innerHTML
+            
+            let totalTwo = squares[i + width].innerHTML
+            
+            let totalThree = squares[i + (width * 2)].innerHTML
+            
+            let totalFour = squares[i + (width * 3)].innerHTML
+            console.log(totalFour)
+            let column = [parseInt(totalOne) + parseInt(totalTwo) + parseInt(totalThree) + parseInt(totalFour)]
+            console.log(column)
+            let filteredColumn = column.filter(num => num)
 
-                squares[i].innerHTML = newColumn[0]
-                squares[i + width].innerHTML = newColumn[1]
-                squares[i + (width * 2)].innerHTML = newColumn[2]
-                squares[i + (width * 3)].innerHTML = newColumn[3]
-            }
+            let missing = 4 - filteredColumn.length
+            let zeros = Array(missing).fill(0)
+            let newColumn = filteredColumn.concat(zeros)
+
+            squares[i].innerHTML = newColumn[0]
+            squares[i + width].innerHTML = newColumn[1]
+            squares[i + (width * 2)].innerHTML = newColumn[2]
+            squares[i + (width * 3)].innerHTML = newColumn[3]
+
         }
 
     }
@@ -194,6 +199,7 @@ document.addEventListener('DOMContentLoaded', () => {
         for (let i = 0; i < squares.length; i++) {
             if (squares[i].innerHTML == 0) {
                 zeros++
+                
             }
         }
         if (zeros === 0) {
