2973c550ff3cc70457cc06047ecd4a04ee65d412
diuguide <61917141+diuguide@users.noreply.github.com>
2020-08-20 18:17:25 -0400

    changing gravity and motion

--- games/jump_n_slide/app.js
+++ games/jump_n_slide/app.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const prince = document.querySelector('.character');
     let bottom = 0
-    let gravity = 0.9
+    let gravity = 0.8
     let isJumping = false
     let isGoingLeft = false
     let isGoingRight = false
@@ -14,58 +14,69 @@ document.addEventListener('DOMContentLoaded', () => {
     function jump() {
 
         if (isJumping) return
+
         prince.classList.add('character')
         prince.classList.remove('character-slide')
+
         let timerUpId = setInterval(() => {
-            if (bottom > 250) {
+            let count = 0
+            if (bottom > 325) {
+
                 clearInterval(timerUpId)
+
                 let timerDownId = setInterval(() => {
+
                     if (bottom < 0) {
                         clearInterval(timerDownId)
                         isJumping = false
                     }
-                    bottom -= 5
+
+                    bottom -= 20
                     prince.style.bottom = bottom + 'px'
+
                 }, 20)
             }
+
             isjumping = true
-            bottom += 30
+            bottom += 100
             bottom = bottom * gravity
             prince.style.bottom = bottom + 'px'
+
         }, 20)
+
     }
-    function slideLeft() {
-        prince.classList.add('character-slide')
-        prince.classList.remove('character')
-        if (isGoingRight) {
-            clearInterval(rightTimerId)
-            isGoingRight = false
-        }
-        isGoingLeft = true
-        leftTimerId = setInterval(() => {
-            left -= 5
-            console.log('going left')
-            prince.style.left = left + 'px'
-        }, 20)
-        left -= 5
-        prince.style.left = left + 'px'
-    }
-    function slideRight() {
-        prince.classList.add('character-slide')
-        prince.classList.remove('character')
-        if (isGoingLeft) {
-            clearInterval(leftTimerId)
-            isGoingLeft = false
-        }
-        isGoingRight = true
-        rightTimerId = setInterval(() => {
-            left += 5
-            console.log('going right')
-            prince.style.left = left + 'px'
-        }, 20)
-        left -= 5
-        prince.style.left = left + 'px'
-    }
+    // function slideLeft() {
+    //     prince.classList.add('character-slide')
+    //     prince.classList.remove('character')
+    //     if (isGoingRight) {
+    //         clearInterval(rightTimerId)
+    //         isGoingRight = false
+    //     }
+    //     isGoingLeft = true
+    //     leftTimerId = setInterval(() => {
+    //         left -= 5
+    //         console.log('going left')
+    //         prince.style.left = left + 'px'
+    //     }, 20)
+    //     left -= 5
+    //     prince.style.left = left + 'px'
+    // }
+    // function slideRight() {
+    //     prince.classList.add('character-slide')
+    //     prince.classList.remove('character')
+    //     if (isGoingLeft) {
+    //         clearInterval(leftTimerId)
+    //         isGoingLeft = false
+    //     }
+    //     isGoingRight = true
+    //     rightTimerId = setInterval(() => {
+    //         left += 5
+    //         console.log('going right')
+    //         prince.style.left = left + 'px'
+    //     }, 20)
+    //     left -= 5
+    //     prince.style.left = left + 'px'
+    // }
     function control(e) {
         if (e.keyCode === 38) {
             jump()
