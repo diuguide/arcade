706617675e09da74597df0bfe8b2e273c58dc0ce
diuguide <61917141+diuguide@users.noreply.github.com>
2020-08-20 16:46:20 -0400

    jump, control functions

--- games/jump_n_slide/app.js
+++ games/jump_n_slide/app.js
@@ -1,15 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     const prince = document.querySelector('.character');
     let bottom = 0
+
+
+
     function jump() {
-        bottom += 30
-        prince.style.bottom = bottom + 'px'
+        let timerId = setInterval(() => {
+            bottom += 30
+            prince.style.bottom = bottom + 'px'
+        }, 20)
     }
-    jump()
-
-
     function control(e) {
-
+        if (e.keyCode === 38) {
+            jump()
+        }
     }
-
+    document.addEventListener('keydown', control)
 })
