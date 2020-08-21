602b28897cb9007a63aab35c7fa5e256442d920a
diuguide <61917141+diuguide@users.noreply.github.com>
2020-08-20 21:46:02 -0400

    block jumps across the screen

--- games/jump_n_slide/app.js
+++ games/jump_n_slide/app.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const prince = document.querySelector('.character');
-    let bottom = 0
-    let gravity = 0.8
+    let bottom = 300
+    let gravity = 0.99
+    let parachute = 1.1
     let isJumping = false
     let isGoingLeft = false
     let isGoingRight = false
@@ -19,64 +20,78 @@ document.addEventListener('DOMContentLoaded', () => {
         prince.classList.remove('character-slide')
 
         let timerUpId = setInterval(() => {
-            let count = 0
-            if (bottom > 325) {
+
+            if (bottom > 300) {
 
                 clearInterval(timerUpId)
 
                 let timerDownId = setInterval(() => {
-
-                    if (bottom < 0) {
+                    console.log(bottom)
+                    if (bottom <= 0) {
                         clearInterval(timerDownId)
                         isJumping = false
                     }
 
-                    bottom -= 20
+                    bottom -= 35
+                    bottom = bottom * parachute
                     prince.style.bottom = bottom + 'px'
 
                 }, 20)
             }
 
             isjumping = true
-            bottom += 100
+            bottom += 40
             bottom = bottom * gravity
             prince.style.bottom = bottom + 'px'
 
         }, 20)
 
+        console.log(bottom)
     }
-    // function slideLeft() {
-    //     prince.classList.add('character-slide')
-    //     prince.classList.remove('character')
-    //     if (isGoingRight) {
-    //         clearInterval(rightTimerId)
-    //         isGoingRight = false
-    //     }
-    //     isGoingLeft = true
-    //     leftTimerId = setInterval(() => {
-    //         left -= 5
-    //         console.log('going left')
-    //         prince.style.left = left + 'px'
-    //     }, 20)
-    //     left -= 5
-    //     prince.style.left = left + 'px'
-    // }
-    // function slideRight() {
-    //     prince.classList.add('character-slide')
-    //     prince.classList.remove('character')
-    //     if (isGoingLeft) {
-    //         clearInterval(leftTimerId)
-    //         isGoingLeft = false
-    //     }
-    //     isGoingRight = true
-    //     rightTimerId = setInterval(() => {
-    //         left += 5
-    //         console.log('going right')
-    //         prince.style.left = left + 'px'
-    //     }, 20)
-    //     left -= 5
-    //     prince.style.left = left + 'px'
-    // }
+    function slideLeft() {
+        prince.classList.add('character-slide')
+        prince.classList.remove('character')
+        if (isGoingRight) {
+            clearInterval(rightTimerId)
+            isGoingRight = false
+        }
+        isGoingLeft = true
+        leftTimerId = setInterval(() => {
+            left -= 5
+            console.log('going left')
+            prince.style.left = left + 'px'
+        }, 20)
+        clearInterval(leftTimerId)
+        left -= 5
+        prince.style.left = left + 'px'
+        console.log('left' + bottom)
+    }
+    function slideRight() {
+        prince.classList.add('character-slide')
+        prince.classList.remove('character')
+        if (isGoingLeft) {
+            clearInterval(leftTimerId)
+            isGoingLeft = false
+        }
+        isGoingRight = true
+
+
+        rightTimerId = setInterval(() => {
+            if (left === 500) {
+                clearInterval(rightTimerId)
+            }
+            left += 20
+            left = left * gravity
+            console.log('going right')
+            prince.style.left = left + 'px'
+        }, 20)
+
+        // if (bottom <= 0) { clearInterval(rightTimerId) }
+
+        left += 5
+        prince.style.left = left + 'px'
+        console.log('right' + bottom)
+    }
     function control(e) {
         if (e.keyCode === 38) {
             jump()
@@ -86,5 +101,6 @@ document.addEventListener('DOMContentLoaded', () => {
             slideRight()
         }
     }
+    console.log('bottom' + bottom)
     document.addEventListener('keydown', control)
 })
