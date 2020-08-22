41259a5d7953d6a7d9ef0e2544a43c24ec17692b
diuguide <61917141+diuguide@users.noreply.github.com>
2020-08-21 23:01:56 -0400

    generates random color background for 64 tiles

--- games/candycrush/app.js
+++ games/candycrush/app.js
@@ -2,18 +2,21 @@ document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     const width = 8
     const squares = []
-
     const candyColors = [
         'red',
         'yellow',
         'orange',
         'green',
         'purple',
-        'blue',
+        'blue'
     ]
+
     function createBoard() {
-        for (let i= 0; i < width * width; i++) {
-            const square = createElement('div')
+        for (let i = 0; i < width * width; i++) {
+            const square = document.createElement('div')
+            let randomColor = Math.floor(Math.random() * candyColors.length)
+            console.log(randomColor)
+            square.style.backgroundColor = candyColors[randomColor]
             grid.appendChild(square)
             squares.push(square)
         }
