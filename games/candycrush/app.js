f474bb713dc8362e3eef96b418716bb19f2a9c73
diuguide <61917141+diuguide@users.noreply.github.com>
2020-08-21 23:16:02 -0400

    added event listeners for movement of drags

--- games/candycrush/app.js
+++ games/candycrush/app.js
@@ -14,6 +14,8 @@ document.addEventListener('DOMContentLoaded', () => {
     function createBoard() {
         for (let i = 0; i < width * width; i++) {
             const square = document.createElement('div')
+            square.setAttribute('draggable', true)
+            square.setAttribute('id', i)
             let randomColor = Math.floor(Math.random() * candyColors.length)
             console.log(randomColor)
             square.style.backgroundColor = candyColors[randomColor]
@@ -22,4 +24,46 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
     createBoard()
+
+    squares.forEach(square => square.addEventListener('dragstart', dragStart))
+    squares.forEach(square => square.addEventListener('dragend', dragEnd))
+    squares.forEach(square => square.addEventListener('dragover', dragOver))
+    squares.forEach(square => square.addEventListener('dragenter', dragEnter))
+    squares.forEach(square => square.addEventListener('dragleave', dragLeave))
+    squares.forEach(square => square.addEventListener('drop', dragDrop))
+
+    function dragStart() {
+        console.log(this.id, 'dragstart')
+
+    }
+    function dragOver() {
+        console.log(this.id, 'dragover')
+
+    }
+    function dragEnter() {
+        console.log(this.id, 'dragenter')
+
+    }
+    function dragLeave() {
+        console.log(this.id, 'dragleave')
+
+    }
+    function dragEnd() {
+        console.log(this.id, 'dragend')
+
+    }
+    function dragDrop() {
+        console.log(this.id, 'drop')
+
+    }
+
+
+
+
+
+
+
+
+
+
 })
