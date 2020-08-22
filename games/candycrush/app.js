1fa8c704ab5d7273968a0b76ef04085e0697a984
diuguide <61917141+diuguide@users.noreply.github.com>
2020-08-22 00:45:45 -0400

    check row for four

--- games/candycrush/app.js
+++ games/candycrush/app.js
@@ -87,6 +87,9 @@ document.addEventListener('DOMContentLoaded', () => {
             let decidedColor = squares[i].style.backgroundColor
             const isBlank = squares[i].style.backgroundColor === ''
 
+            const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55]
+            if (notValid.includes(i)) continue
+
             if (rowOfThree.every(index => squares[index].style.backgroundColor === decidedColor && !isBlank)) {
                 score += 3
                 rowOfThree.forEach(index => {
@@ -109,11 +112,43 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }
     }
+    function checkRowForFour() {
+        for (i = 0; i < 60; i++) {
+            let rowOfFour = [i, i + 1, i + 2, i + 3]
+            let decidedColor = squares[i].style.backgroundColor
+            const isBlank = squares[i].style.backgroundColor === ''
+
+            const notValid = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55]
+            if (notValid.includes(i)) continue
+
+            if (rowOfFour.every(index => squares[index].style.backgroundColor === decidedColor && !isBlank)) {
+                score += 3
+                rowOfFour.forEach(index => {
+                    squares[index].style.backgroundColor = ''
+                })
+            }
+        }
+    }
+    function checkColumnForFour() {
+        for (i = 0; i < 46; i++) {
+            let columnOfFour = [i, i + width, i + width * 2, i + width * 3]
+            let decidedColor = squares[i].style.backgroundColor
+            const isBlank = squares[i].style.backgroundColor === ''
+
+            if (columnOfFour.every(index => squares[index].style.backgroundColor === decidedColor && !isBlank)) {
+                score += 3
+                columnOfFour.forEach(index => {
+                    squares[index].style.backgroundColor = ''
+                })
+            }
+        }
+    }
 
     checkColumnForThree()
     checkRowForThree()
 
     window.setInterval(() => {
+        checkRowForFour()
         checkColumnForThree()
         checkRowForThree()
     }, 100)
